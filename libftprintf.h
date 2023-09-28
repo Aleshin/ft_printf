62938b8999e82aa332438b1a805d41be69dc7d00
Sergey Aleshin <saleshin@cbr3s2.42barcelona.com>
2023-09-28 18:54:36 +0200

    start

---
+++ libftprintf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saleshin <saleshin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 15:43:13 by saleshin          #+#    #+#             */
+/*   Updated: 2023/09/28 15:43:17 by saleshin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+
+#endif
